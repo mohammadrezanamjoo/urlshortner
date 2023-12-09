b5572355487a015df24cdcc54088517f6c997cc7
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2023-12-09 17:44:09 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/urlshortener', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+// Define URL schema
+const urlSchema = new mongoose.Schema({
+  originalUrl: { type: String, required: true },
+  shortUrl: { type: String, required: true, unique: true },
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Endpoint to shorten a URL
+app.post('/shorten', async (req, res) => {
+  const { originalUrl } = req.body;
+
+  // Check if the URL is valid
+  if (!isValidUrl(originalUrl)) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
+
+  try {
+    // Check if the URL is already in the database
+    let url = await Url.findOne({ originalUrl });
+
+    if (!url) {
+      // Generate a short URL using shortid
+      const shortUrl = shortid.generate();
+
+      // Create a new URL entry in the database
+      url = new Url({ originalUrl, shortUrl });
+      await url.save();
+    }
+
+    res.json({ shortUrl: url.shortUrl });
+  } catch (error) {
+    console.error('Error shortening URL:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Endpoint to expand a short URL
+app.get('/:shortUrl', async (req, res) => {
+  const { shortUrl } = req.params;
+
+  try {
+    // Find the original URL based on the short URL
+    const url = await Url.findOne({ shortUrl });
+
+    if (url) {
+      res.redirect(url.originalUrl);
+    } else {
+      res.status(404).json({ error: 'Short URL not found' });
+    }
+  } catch (error) {
+    console.error('Error expanding URL:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Utility function to check if a URL is valid
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
